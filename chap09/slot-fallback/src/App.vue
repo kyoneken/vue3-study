12db6df265a1cd376dc07b838c364d5d37449ee0
kyoneken <spry.car7536@fastmail.com>
2024-06-13 23:57:34 +0000

    slotのフォールバックコンテンツ

---
+++ chap09/slot-fallback/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import OneSection from './components/OneSection.vue';
+
+const taro = ref('Taro Tanaka');
+const hanako = ref('Hanako Yamada');
+</script>
+
+<template>
+<section>
+  <h2>Slotの利用</h2>
+  <OneSection v-bind:name="taro">
+    <p>連絡がつきません。</p>
+  </OneSection>
+  <OneSection v-bind:name="hanako" />
+</section>
+</template>
---
+++ chap09/slot-fallback/src/components/OneSection.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+    name: string
+}
+defineProps<Props>();
+</script>
+
+<template>
+    <section class="box">
+        <h1>{{ name }}さんの状況</h1>
+        <slot>
+            <p>情報がありません。</p>
+        </slot>
+    </section>
+</template>
+<style>
+.box {
+    border: greenq 1px solid;
+    margin: auto;
+}
+</style>
